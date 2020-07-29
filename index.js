e1087839d9655203d34dba66aad97a46dc7a8398
Victor <vctrd3@gmail.com>
2020-07-29 11:20:12 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+
+const button = document.querySelector('button')
+const form = document.querySelector('form')
+const userInput = document.querySelector('#input')
+const result = document.querySelector('#result')
+const error = document.querySelector('#error')
+const loader = document.querySelector('.loader');
+
+const getEightBallAnswer = async (userQuestion) => {
+  try{
+     
+    if(userQuestion.length === 0){
+      return errorMessage("Please enter a question")
+    }else if(userQuestion[userQuestion.length -1] !== '?'){
+      return errorMessage('Your question must end with a question mark')
+    }
+    errorMessage("")
+    
+    const baseURI = 'https://8ball.delegator.com/magic/JSON/'
+    loader.classList.add('show');
+    const res = await fetch(baseURI + userQuestion)
+    const data = await res.json()
+    
+    displayResult(data)
+    loader.classList.remove('show');
+    form.reset()
+    }catch(err){
+      loader.classList.remove('show');
+      return errorMessage('Invalid request')
+      
+  }
+}
+
+const displayResult = (data) => {
+  result.textContent = data.magic.answer
+}
+
+const errorMessage = (msg) =>{
+  error.innerHTML = msg
+}
+
+button.addEventListener('click', () => {
+  getEightBallAnswer(userInput.value)
+})
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  getEightBallAnswer(userInput.value)
+})
